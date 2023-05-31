1cf96cc34c28c1378bc9f7f77079c950d47b8dbe
nioke-dev <nioke8090@gmail.com>
2023-05-31 15:34:50 +0700

    jobsheet Asd tree percobaan 1

---
+++ src/Tree/BinaryTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree(){
+        root = null;
+    }
+
+    boolean isEmpty(){
+        return root == null;
+    }
+
+}
